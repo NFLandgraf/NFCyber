71926bce5155ff3a62e218e1c86625e0c423192e
Nico Landgraf <nicolas.landgraf@web.de>
2024-03-20 21:51:11 +0100

    reorder

---
+++ zLearning/Stereotaxe/internal_info.py
@@ -0,0 +1,11 @@
+# user input:
+ocheck_size_comp = False
+ocheck_AP_tilt = False
+ocheck_ML_tilt = True
+onormal_lmd = [-3.8, 0, 0]
+oraw_aim = [-1.8, 0.2, -2.5]
+oaim = [-1.8, 0.2, -2.5]
+omeasured_lmd = [-3.8, 0, -0.2]
+omeasured_ML_R = [0, 1.5, 0.263]
+omeasured_ML_L = [0, -1.5, -0.564]
+
