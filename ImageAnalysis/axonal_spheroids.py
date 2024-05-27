0efaac3d2f52693a6b252e5929a3754b24734793
NFLandgraf <nicolas.landgraf@web.de>
2024-05-27 18:18:02 +0200

    a

--- ImageAnalysis/axonal_spheroids.py
+++ ImageAnalysis/axonal_spheroids.py
@@ -2,6 +2,7 @@
 '''
 - lay a circle with a fixed radius on each pixel 
 - save the pixels where the number of white pixel in that circle is above a threshold
+- scaling 20x: 0.32um/px, 10x: 0.65um/px, 2.5x: 2.58um/px
 
 '''
 
@@ -17,16 +18,29 @@ import math
 path = 'C:\\Users\\landgrafn\\NFCyber\\ImageAnalysis\\data\\'
 
 
-wt = ['196', '198', '200', '206', '209', '211']
-app = ['706', '707', '711', '767', '768', '777']
+min_AS_diam_um = 4
+threshold = 0.95  # in decimals
+
+wt = ['198', '200', '206']
+app = ['767', '768', '777']
+
+
+
+
+
+
+scaling_measure = 0.32  # um/px
+radius = int((min_AS_diam_um / scaling_measure) / 2)
+print(f'radius in um/px: {min_AS_diam_um} / {radius}\n'
+      f'threshold: {threshold}\n')
+
+px_in_kernel = [0,5,13,29,49,81,113,149,197,253,317]
+threshold = int(threshold * px_in_kernel[radius])
+
 groups = [wt, app]
 
 NET_wt = [7.28, 5.95, 7.09, 3.52, 4.18, 3.67]
 NET_app = [3.9, 2.67, 4.05, 3.25, 3.81, 9.58]
-
-radii = [10]
-thresholds = [200]
-
 
 def get_files(path, common_name):
     # get files
@@ -41,6 +55,7 @@ def show_image(img):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+
 def get_accumulations(pic, radius, threshold):
 
     # load image in grayscale and binarize (0.0/1.0) (white_px = high values, black_px = low values)
@@ -49,12 +64,10 @@ def get_accumulations(pic, radius, threshold):
     image = image // 255
     image = image.astype(np.float32)
 
-    # Create a disk-shaped kernel [0,1,0,...], lay kernel above each px and sum the 1 values around px 
+    # Create a disk-shaped kernel [0,1,0,...], lay kernel above each px and sum the '1' values around px 
     kernel = disk(radius)
     convolution_result = cv2.filter2D(image, cv2.CV_32F, kernel)    # sum of surrounding px values
     mask = convolution_result > threshold   # saying True/False if sum(neighbour_px) > threshold
-
-    show_image(mask)
 
     # get results
     nb_white_acc, labels_im = cv2.connectedComponents(mask.astype(np.uint8))
@@ -64,25 +77,21 @@ def get_accumulations(pic, radius, threshold):
     return nb_white_acc, nb_white_px
 
 def check_animal(common_name, radius, threshold):
-
     # go through all files of an animal and return the animals' mean
-    acc_list = []
-    px_list = []
 
     files = get_files(path, common_name)
-    for file in files:
-        nb_white_acc, nb_white_px = get_accumulations(file, radius, threshold)
-        acc_list.append(nb_white_acc)
-        px_list.append(nb_white_px)
+    acc_list, px_list = zip(*(get_accumulations(file, radius, threshold) for file in files))
 
-    return np.mean(px_list)
+    return np.mean(acc_list), np.mean(px_list)
 
 def check_group(animal_list, radius, threshold):
-    # go through animal group (e.g. WT) and return its values
-    px_means = np.array([check_animal(animal, radius, threshold) for animal in animal_list])
+    # go through animal group (e.g. WT) and return its mean values
+    
+    acc_means, px_means = zip(*(check_animal(animal, radius, threshold) for animal in animal_list))
 
-    return px_means
+    return acc_means, px_means
 
+'''
 def calc_stats(radius, threshold):
 
     # devide by the %area of NET to normalize it
@@ -113,21 +122,11 @@ def main():
 
     return all_values
 
+'''
 
-all_values = main()
-print(all_values)
-
-
-
-#%%
-
-all_p = [values[0] for values in all_values]
-
-sig = min(all_p)
-print(sig)
-
-sig_index = all_p.index(sig)
-print(all_p[sig_index])
-
-print(all_values[sig_index])
-
+# divide by the %area of NET to normalize it
+for group in groups:
+    acc_means, px_means = check_group(group, radius, threshold)
+    print(acc_means)
+    print(px_means)
+    
