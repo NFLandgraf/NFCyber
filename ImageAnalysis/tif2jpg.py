f4d96073ad557333d1bece4b56dc829fbfc59041
NFLandgraf <nicolas.landgraf@web.de>
2024-10-25 18:33:24 +0200

    a

--- ImageAnalysis/tif2jpg.py
+++ ImageAnalysis/tif2jpg.py
@@ -3,25 +3,68 @@ import os
 from PIL import Image
 
 
-path = 'C:\\Users\\landgrafn\\Desktop\\aa\\'
+path = 'C:\\Users\\landgrafn\\Desktop\\asa\\'
 
 
 for root, dirs, files in os.walk(path, topdown=False):
     for name in files:
         print(os.path.join(root, name))
+        print('\n')
         if os.path.splitext(os.path.join(root, name))[1].lower() == ".tiff":
             if os.path.isfile(os.path.splitext(os.path.join(root, name))[0] + ".jpg"):
                 print("A jpeg file already exists")
 
 
+
             # If a jpeg is *NOT* present, create one from the tiff.
             else:
                 outfile = os.path.splitext(os.path.join(root, name))[0] + ".jpg"
-                try:
-                    im = Image.open(os.path.join(root, name))
-                    print("Generating jpeg")
-                    #im.thumbnail(im.size)
-                    print(outfile)
-                    im.save(outfile, "JPEG", quality=100)
-                except Exception:
-                    print('nay')
+                
+                im = Image.open(os.path.join(root, name))
+                print("Generating jpeg")
+                #im.thumbnail(im.size)
+
+                print(outfile)
+                im.save('C:\\Users\\landgrafn\\Desktop\\asa\\aa.png')
+                print('saved')
+
+
+
+#%%
+
+import os
+from PIL import Image
+import numpy as np
+import tifffile as tiff  # Optional if using tifffile for loading TIFFs
+
+path = 'C:\\Users\\landgrafn\\Desktop\\asa\\'
+
+for root, dirs, files in os.walk(path, topdown=False):
+    for name in files:
+        print(os.path.join(root, name))
+        print('\n')
+        if os.path.splitext(os.path.join(root, name))[1].lower() == ".tiff":
+            if os.path.isfile(os.path.splitext(os.path.join(root, name))[0] + ".jpg"):
+                print("A JPEG file already exists")
+            else:
+                outfile = os.path.splitext(os.path.join(root, name))[0] + ".jpg"
+                
+                # Open the image and handle floating-point or 16-bit data
+                im = Image.open(os.path.join(root, name))
+                
+                # Handle floating-point or 16-bit data by converting to 8-bit
+                if im.mode == 'F':  # Floating-point mode
+                    print("Converting from floating-point mode to 8-bit.")
+                    np_image = np.array(im)
+                    np_image = (255 * (np_image - np_image.min()) / np_image.ptp()).astype(np.uint8)
+                    im = Image.fromarray(np_image)
+                
+                elif im.mode == 'I;16':  # 16-bit mode
+                    print("Converting from 16-bit mode to 8-bit.")
+                    np_image = np.array(im, dtype=np.uint16)
+                    np_image = (np_image / 256).astype(np.uint8)
+                    im = Image.fromarray(np_image)
+
+                print("Generating JPEG")
+                im.save('C:\\Users\\landgrafn\\Desktop\\asa\\aa.png')
+                print('Saved as PNG:', 'C:\\Users\\landgrafn\\Desktop\\asa\\aa.png')
