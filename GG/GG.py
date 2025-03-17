fb50aff69189385dced609a5fdc0d0af6ad7e984
Nico Landgraf <nicolas.landgraf@web.de>
2025-03-17 19:18:37 +0100

    gg

--- GG/GG.py
+++ GG/GG.py
@@ -3,8 +3,9 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.stats import zscore
+import numpy as np
 
-df = pd.read_excel('C:\\Users\\landgrafn\\Desktop\\aa.xlsx', sheet_name='Tabelle1')  # Passe den Pfad und Sheet-Namen an
+df = pd.read_excel('C:\\Users\\nicol\\Desktop\\GG.xlsx', sheet_name='Tabelle1')
 
 df = df.set_index('ID')
 
@@ -14,19 +15,36 @@ df_Iba = df.filter(like="Iba1")
 df_GFAP = df.filter(like="GFAP")
 
 
-print(df_AT8)
-# print(df_Iba)
-# print(df_GFAP)
 
 #%%
 
-print(type(df_AT8.loc[200, 'AT8 POA %']))
+AT8_mean = np.mean(df_AT8.values)
+AT8_sd = np.std(df_AT8.values)
+AT8_z = (df_AT8 - AT8_mean) / AT8_sd
 
+Iba_mean = np.mean(df_Iba.values)
+Iba_sd = np.std(df_Iba.values)
+Iba_z = (df_Iba - Iba_mean) / Iba_sd
+
+GFAP_mean = np.mean(df_GFAP.values)
+GFAP_sd = np.std(df_GFAP.values)
+GFAP_z = (df_GFAP - GFAP_mean) / GFAP_sd
 
 
 
 #%%
-df_AT8 = df_AT8.apply(pd.to_numeric, errors='coerce')
 
-df_AT8_z = df_AT8.apply(zscore)
-print(df_AT8_z.var())
+plt.figure(figsize=(6, 4))
+sns.heatmap(AT8_z, cmap="coolwarm", vmin=-1.1, vmax=3.2, annot=False)
+plt.title("AT8_z")
+plt.show()
+
+plt.figure(figsize=(6, 4))
+sns.heatmap(Iba_z, cmap="coolwarm", vmin=-2, vmax=2.8, annot=False)
+plt.title("Iba_z")
+plt.show()
+
+plt.figure(figsize=(6, 4))
+sns.heatmap(GFAP_z, cmap="coolwarm", vmin=-1.5, vmax=3, annot=False)
+plt.title("GFAP_z")
+plt.show()
