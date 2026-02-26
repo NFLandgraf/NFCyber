bbe87064d3a99c0684a3a83bf58490fa7a90aab4
NFLandgraf <nicolas.landgraf@web.de>
2026-02-26 17:31:53 +0100

    vrvre

--- FFAnalysis/CA1Dopa_Analysis_FF.py
+++ FFAnalysis/CA1Dopa_Analysis_FF.py
@@ -6,9 +6,11 @@ import matplotlib.pyplot as plt
 from scipy import signal, optimize, stats
 from pathlib import Path
 import os
+from scipy.ndimage import gaussian_filter1d
 
-path = r"D:\FF\FF-DA_FS"
-file_useless_string = ['2024-11-20_FF-Weilin_', '_FS']
+
+path = r"E:\FF-DA_YM\a"
+file_useless_string = ['2024-11-19_FF-Weilin_YMaze_']#, '_FS']
 
 def manage_filename(file):
 
@@ -34,113 +36,82 @@ files = get_files(path, '.doric')
 
 
 #%%
-def get_distances(main_df, file_doric, bps_position='mid_backend', min_distance=0.001, DLC_mm_per_px = 0.12):
-    # calculates the distance travelled between frames
+def extract_signals_1IO(file_doric):
+    # do this when you have 1 IO-TTLs, e.g. IO2-Cam_TTL
+
+    def h5print(item, leading=''):
+        # prints out the h5 data structure
+        for key in item:
+            if isinstance(item[key], h5py.Dataset):
+                print(leading + key + ':' + str(item[key].shape))
+            else:
+                print(leading + key)
+                h5print(item[key], leading + '  ')
     
-    def add_distances_to_maindf(main_df, df_distances, digi_Cam_column='digi_Cam'):
-        # Take a column from the main_df and the df of the movements between frames
-        # Now in which row here is a one in the Cam_TTL_column, add the corresponding travel distance
-
-        timepoint_frame = main_df.index[main_df[digi_Cam_column] == 1]    
-
-        # check that Cam_frames and Cam_TTL are the same
-        nb_frames_TTL = len(timepoint_frame)
-        nb_frames_DLC = df_distances.shape[0]
-        #print(f'nb_frames_DLC/nb_frames_TTL -> {nb_frames_DLC}/{nb_frames_TTL}')
-
-        # if the number of TTL and frames dont mach up, just add some more distances/frames with distance=0
-        while nb_frames_DLC != nb_frames_TTL:
-            print(f'ALARM!!! {nb_frames_DLC}/{nb_frames_TTL}')
-            df_distances.loc[len(df_distances), 'mid_backend_dist'] = 0
-            nb_frames_DLC = df_distances.shape[0]
-
-        # add the distances to the main dataframe, whereever there is a 1 at the respective IO
-        main_df['Distances'] = np.nan
-        main_df.loc[timepoint_frame, 'Distances'] = df_distances.iloc[:len(timepoint_frame)].values.flatten()
-
-        return main_df
-
-    def cleaning_raw_df(file_DLC):
-
-        df = pd.read_csv(file_DLC, header=None, low_memory=False)
-
-        # create new column names from first two rows
-        new_columns = [f"{col[0]}_{col[1]}" for col in zip(df.iloc[1], df.iloc[2])]
-        df.columns = new_columns
-        df = df.drop(labels=[0, 1, 2], axis="index")
-
-        # set index and convert to numeric
-        df.set_index('bodyparts_coords', inplace=True)
-        df.index.names = ['frames']
-        df = df.astype(float)
-        df.index = df.index.astype(int)
-
-        bps_all = ['nose', 'left_ear', 'right_ear', 'left_ear_tip', 'right_ear_tip', 'left_eye', 'right_eye', 'head_midpoint', 
-                'neck', 'mid_back', 'mouse_center', 'mid_backend', 'mid_backend2', 'mid_backend3', 
-                'tail_base', 'tail1', 'tail2', 'tail3', 'tail4', 'tail5', 'tail_end',
-                'left_shoulder', 'left_midside', 'left_hip', 'right_shoulder', 'right_midside', 'right_hip']
-
-        # remove low-confidence points
-        for bodypart in bps_all:
-            filter = df[f'{bodypart}_likelihood'] <= 0.9
-            df.loc[filter, f'{bodypart}_x'] = np.nan
-            df.loc[filter, f'{bodypart}_y'] = np.nan
-            df = df.drop(columns=f'{bodypart}_likelihood')
-
+    def extend_high_signal(df, column, extra_rows):
+        # for one IO channel, extend the ocurrence of 1 to also see the high state when downsampling to 100Hz
+        # Extend the 1s for x additional rows after a transition
+        
+        transitions = (df[column].shift(1) == 1) & (df[column] == 0)  # where does it change from 1 to 0
+        for i in range(len(df)):
+            if transitions.iloc[i]:  # If there was a transition from 1 to 0
+                for j in range(0, extra_rows + 1):  # Add `1` for x extra rows
+                    if i + j < len(df):  # Ensure we stay within bounds
+                        df.iloc[i + j, df.columns.get_loc(column)] = 1
         return df
-    
-    def euclidian_dist(point1x, point1y, point2x, point2y):
-        # calculates distance between 2 points in carthesic coordinate system
-        return np.sqrt((point1x - point2x) ** 2 + (point1y - point2y) ** 2)
 
-    file_DLC = str(file_doric).replace('.doric', '_DLC.csv')
-    df = cleaning_raw_df(file_DLC)
-    x_col, y_col = f'{bps_position}_x', f'{bps_position}_y'
-    
-    # creates a full index range (to include missing frames), fill Series with NaNs
-    full_index = np.arange(df.index.min(), df.index.max() + 1)
-    dist_series = pd.Series(index=full_index, dtype=float)
+    def get_hightimes(df_io, value_threshold=0.9):
+        # returns a list with the onset (and offset) timepoints, where certain channel is 'high' 
+        s = df_io['digi_Cam'].to_numpy()
+        t = df_io.index.to_numpy()
 
-    # goes though all frames/rows and calcs distance between the frames with values (not NaN)
-    last_valid_idx = None
-    for i in df.index:
-        if not np.isnan(df.at[i, x_col]) and not np.isnan(df.at[i, y_col]):
-            if last_valid_idx is not None:
-                dist = euclidian_dist(df.at[last_valid_idx, x_col], df.at[last_valid_idx, y_col], df.at[i, x_col], df.at[i, y_col])
-                dist_series[i] = dist if dist >= min_distance else np.nan  
-            last_valid_idx = i
-    dist_travelled = round(dist_series.sum(), 1)
+        high = s >= value_threshold
+        edges = np.diff(high.astype(np.int8))
 
-    # goes through the frames and distributes the distance evenly across NaNs
-    nan_indices = np.where(dist_series.isna())[0]
-    if len(nan_indices) > 0:
-        last_valid_idx = None
-        for i in range(len(dist_series)):
-            if not np.isnan(dist_series[i]):  
-                if last_valid_idx is not None:
-                    num_missing = i - last_valid_idx - 1  # Frames between valid distances
-                    if num_missing > 0:
-                        total_dist = dist_series[i]  # Distance to be spread
-                        per_frame_dist = total_dist / (num_missing + 1)
-                        for j in range(1, num_missing + 2):
-                            dist_series[last_valid_idx + j] = per_frame_dist
-                last_valid_idx = i
-    dist_series[0] = 0.0
-    dist_travelled_distr = round(dist_series.sum(), 1)
-    if dist_travelled != dist_travelled_distr:
-        print(f'Error: different distances calculated: {dist_travelled} vs {dist_travelled_distr}')
-    
-    # convert to mm and to DataFrame
-    dist = dist * DLC_mm_per_px
-    df_distances = dist_series.to_frame(name=f'{bps_position}_dist')
-    #print(f'distance travelled: {round(df_distances.sum(), 1)}mm')
-    
-    main_df = add_distances_to_maindf(main_df, df_distances)
+        onset_idx  = np.where(edges == 1)[0] + 1   # low→high
+        offset_idx = np.where(edges == -1)[0] + 1  # high→low
 
-    return main_df
+        hightimes = np.c_[t[onset_idx], t[offset_idx]]
+        onsets = np.array(hightimes)[:, 0]
+        diffs = np.diff(onsets)
+
+        # unique_vals, counts = np.unique(diffs, return_counts=True)
+        # for val, count in zip(unique_vals, counts):
+        #     print(f"{val} → {count}")
+
+        return hightimes
+
+
+    with h5py.File(file_doric, 'r') as f:
+        #h5print(f, '')     # prints the whole structure
+        path = 'DataAcquisition/NC500/Signals/Series0001/'
+
+        # collect raw data from h5 file
+        sig_isos  = np.array(f[path + 'LockInAOUT01/AIN01'])
+        sig_fluo  = np.array(f[path + 'LockInAOUT02/AIN01'])
+        sig_time  = np.array(f[path + 'LockInAOUT01/Time'])
+
+        digi_Cam    = np.array(f[path + 'DigitalIO/DIO02']).astype(int)
+        digi_time   = np.array(f[path + 'DigitalIO/Time'])
+
+    # get signal parameters
+    sig_time = np.round(sig_time, 2)
+    duration = max(sig_time)
+    sig_sampling_rate = int(len(sig_isos) / duration)
+
+    # create frames
+    df_signal  = pd.DataFrame({'Isos': sig_isos, 'Fluo': sig_fluo}, index=pd.Index(sig_time, name='Time'))
+    df_io      = pd.DataFrame({'digi_Cam': digi_Cam}, index=pd.Index(digi_time, name='Time'))
+
+    # get IO parameters
+    df_io = df_io.iloc[0::10]      # downsample to take every 10th frame -> one datapoint every ms
+    #io = extend_high_signal(io, 'digi_Cam', 6)   # increase the duration to see every TTL at 100Hz
+    io_hightimes = get_hightimes(df_io)
+
+    return df_signal, df_io, io_hightimes
 
 def extract_signals_2IO(file_doric):
-    # do this when you have 2 IOs, e.g. IO2-Cam_TTL and IO3-FCbox
+    # do this when you have 2 IO-TTLs, e.g. IO2-Cam_TTL and IO3-FCbox
 
     def h5print(item, leading=''):
         # prints out the h5 data structure
@@ -181,7 +152,7 @@ def extract_signals_2IO(file_doric):
         return df
 
     with h5py.File(file_doric, 'r') as f:
-        #h5print(f, '')
+        h5print(f, '')     # prints the whole structure
         path = 'DataAcquisition/NC500/Signals/Series0001/'
 
         # collect raw data from h5 file
@@ -218,7 +189,6 @@ def extract_signals_2IO(file_doric):
     events_digi_Cam =   event_list(df, 'digi_Cam')
     events_digi_Shock = event_list(df, 'digi_Shock')
 
-
     # print(  f'Datapoints: Time {len(sig_time)}, Isos {len(sig_isos)}, Fluo {len(sig_fluo)}\n'
     #         f'Datapoints: digi_Cam {len(digi_Cam)}, digi_Shock {len(digi_Shock)}\n'
     #         f'Duration: {duration}s, SamplingRate: {sig_sampling_rate}Hz\n'
@@ -226,6 +196,69 @@ def extract_signals_2IO(file_doric):
     #         f'digi_Shock: {len(events_digi_Shock)}x {[ev[0] for ev in events_digi_Shock]}')
     
     return df, events_digi_Cam, events_digi_Shock
+
+def get_movement(file_DLC, dist_bp='postbody', DLC_mm_per_px = 0.12, fps=30):
+    # calculates the distance and speed between frames
+    
+    def cleaning_raw_df(file_DLC):
+
+        df = pd.read_csv(file_DLC, header=None, low_memory=False)
+
+        # create new column names from first two rows
+        new_columns = [f"{col[0]}_{col[1]}" for col in zip(df.iloc[1], df.iloc[2])]
+        df.columns = new_columns
+        df = df.drop(labels=[0, 1, 2], axis="index")
+        print(len(df))
+
+        # set index and convert to numeric
+        df.set_index('bodyparts_coords', inplace=True)
+        df.index.names = ['frames']
+        df = df.astype(float)
+        df.index = df.index.astype(int)
+
+        bps_all         = ['nose', 'left_ear', 'right_ear', 'left_ear_tip', 'right_ear_tip', 'left_eye', 'right_eye', 'head_midpoint', 
+                            'neck', 'mid_back', 'mouse_center', 'mid_backend', 'mid_backend2', 'mid_backend3', 
+                            'tail_base', 'tail1', 'tail2', 'tail3', 'tail4', 'tail5', 'tail_end',
+                            'left_shoulder', 'left_midside', 'left_hip', 'right_shoulder', 'right_midside', 'right_hip']
+        bps_head        = ['nose', 'left_eye', 'right_eye', 'head_midpoint', 'left_ear', 'right_ear', 'neck']
+        bps_postbody    = ['mid_backend2', 'mid_backend3', 'tail_base']
+
+        # remove low-confidence points
+        for bodypart in bps_all:
+            filter = df[f'{bodypart}_likelihood'] <= 0.9
+            df.loc[filter, f'{bodypart}_x'] = np.nan
+            df.loc[filter, f'{bodypart}_y'] = np.nan
+            df = df.drop(columns=f'{bodypart}_likelihood')
+
+        # interpolate to skip nans
+        df = df.interpolate(method="linear")
+
+        # mean 'trustworthy' bodyparts (ears, ear tips, eyes, midpoint, neck) to 'head' column
+        for c in ('_x', '_y'):
+            df[f'head{c}'] = df[[bp+c for bp in bps_head]].mean(axis=1, skipna=True)
+            df[f'postbody{c}'] = df[[bp+c for bp in bps_postbody]].mean(axis=1, skipna=True)
+
+        # smoothing along time via gaussian filter
+        for col in df.columns:
+            df[col] = gaussian_filter1d(df[col].values, sigma=2, mode="nearest")
+
+        return df
+    
+    df = cleaning_raw_df(file_DLC)
+    
+    x = df[f'{dist_bp}_x'].to_numpy()
+    y = df[f'{dist_bp}_y'].to_numpy()
+
+    dx = np.diff(x)
+    dy = np.diff(y)
+
+    dist = np.sqrt(dx*dx + dy*dy)              # length = n_frames - 1
+    dist = np.r_[0.0, dist]                    # prepend 0 for first frame (align length)
+    dist = dist * DLC_mm_per_px
+    df['Distance'] = dist
+    df['Speed'] = dist * fps
+        
+    return df
 
 def align_to_events(df, events, time_passed_pre=30, time_passed_post=30):
 
@@ -333,22 +366,28 @@ all_animals_event_mean = pd.DataFrame()
 all_animals_ind_event_moves = pd.DataFrame()
 
 
-for i, file in enumerate(files):
-    file_name_short = manage_filename(file)
-    print(file_name_short + '\n')
+for i, file_doric in enumerate(files):
 
-    main_df, events_IO2, events_IO3         = extract_signals_2IO(file)
-    main_df                                 = get_distances(main_df, file)
-    main_df                                 = align_to_events(main_df, events_IO3)  
-    df_dff                                  = dff(main_df, file_name_short)
-    perievent(df_dff, file_name_short)
-    perievent_move(main_df, file_name_short)
+    file_name_short = manage_filename(file_doric)
+    print(f'----- {file_name_short} -----\n')
+
+    file_DLC = str(file_doric).replace('.doric', '_DLC.csv')
+
+
+    df_signal, df_io, io_hightimes = extract_signals_1IO(file_doric)
+    #main_df, events_IO2, events_IO3         = extract_signals_2IO(file)
+
+    df_DLC                                 = get_movement(file_DLC)
+    #main_df                                 = align_to_events(main_df, events_IO3)  
+    #df_dff                                  = dff(main_df, file_name_short)
+    #perievent(df_dff, file_name_short)
+    #perievent_move(main_df, file_name_short)
     
 
-all_animals_dff.to_csv(path + '\\all_animals_dff.csv')
-all_animals_ind_events.to_csv(path + '\\all_animals_ind_events.csv')
-all_animals_event_mean.to_csv(path + '\\all_animals_eventmean.csv')
-all_animals_ind_event_moves.to_csv(path + '\\all_animals_ind_event_moves.csv')
+# all_animals_dff.to_csv(path + '\\all_animals_dff.csv')
+# all_animals_ind_events.to_csv(path + '\\all_animals_ind_events.csv')
+# all_animals_event_mean.to_csv(path + '\\all_animals_eventmean.csv')
+# all_animals_ind_event_moves.to_csv(path + '\\all_animals_ind_event_moves.csv')
 
 #%%
 
